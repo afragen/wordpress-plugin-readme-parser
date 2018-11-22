9d5289acd3fbe07ee96c8f57a854b44557b4e5b0
Andy Fragen <andy@thefragens.com>
2018-11-21 18:51:38 -0800

    message potential failure points

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
 $content = file_get_contents( 'https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-parser.php?format=txt' );
-file_put_contents( 'class-parser.php', $content );
-echo '<h1>class-parser.php updated</h1>';
+if ( ! $content ) {
+	echo '<h2>Error getting file contents</h2>';
+}
+if ( ! file_put_contents( 'class-parser.php', $content ) ) {
+	echo '<h2>Error updating class-parser.php</h2>';
+} else {
+	echo '<h2>class-parser.php updated</h2>';
+}
