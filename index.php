dd56744429cb4c54e2c0ce4d37bb2d829586d39d
Andy Fragen <andy@thefragens.com>
2019-07-01 11:47:36 -0700

    Turn off ssl when working on localhost

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-$content = file_get_contents( 'https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-parser.php?format=txt' );
+$stream_context_options = array(
+	'ssl' => array(
+		'verify_peer'      => false,
+		'verify_peer_name' => false,
+	),
+);
+$content                = file_get_contents( 'https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-parser.php?format=txt', false, stream_context_create( $stream_context_options ) );
 if ( ! $content ) {
 	echo '<h2>Error getting file contents</h2>';
 }
