e0b3b80d4883f840ebe92a5121e379473f0fae35
Andy Fragen <andy@thefragens.com>
2020-07-09 11:10:42 -0700

    update for new user-agent requirement to dot org

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
-$stream_context_options = array(
-	'ssl' => array(
-		'verify_peer'      => false,
-		'verify_peer_name' => false,
-	),
+$stream_context_options = stream_context_create(
+	[
+		'ssl'  => [
+			'verify_peer'      => false,
+			'verify_peer_name' => false,
+		],
+		'http' => [
+			'user_agent' => 'WordPress.org Plugin Readme Parser',
+		],
+	]
 );
-$content                = file_get_contents( 'https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-parser.php?format=txt', false, stream_context_create( $stream_context_options ) );
+
+$content = file_get_contents( 'https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-parser.php?format=txt', false, $stream_context_options );
 if ( ! $content ) {
 	echo '<h2>Error getting file contents</h2>';
 }
